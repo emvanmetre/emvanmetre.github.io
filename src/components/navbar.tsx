c06ff1cb1364f7aa7127ab2c496249018f5b4ca5
Elizabeth Van Metre <elizabeth.vanmetre@vista.com>
2024-08-04 13:41:17 -0400

    feat: create navbar element

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/navbar.css";
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Welcome to GeeksforGeeks</h1>
-        </div>
+        <nav className="navbar">
+            <NavLink to="/" className="nav-logo">
+                NavBar
+            </NavLink>
+        </nav>
     )
 }
 
