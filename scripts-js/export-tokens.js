8518fb7a60fa9e61081f56df7e72dd659b11378e
Elizabeth Van Metre <elizabeth.vanmetre@vista.com>
2024-08-08 14:32:15 -0400

    fix: scss compiling issue

--- scripts-js/export-tokens.js
+++ scripts-js/export-tokens.js
@@ -1,7 +1,4 @@
 const { writeFile } = require("node:fs/promises");
-const sass = require('sass');
-const path = require('path');
-const fs = require('fs');
 const tokens = require('../build/tokens/index');
 
 
@@ -24,26 +21,12 @@ const saveTokens = async (filepath, filename, tokens, filetype) => {
 
 try {
     const cssVariables = tokensToCss(tokens)
+    const cssClass = `:root {\n${cssVariables.replaceAll("--", "  --")}}\n`
+    saveTokens('build', 'tokens', cssClass, 'css')
     const scssClass = cssVariables.replace(/:.*$/gm, '').replace(/^(?=-).*$/gm, (line) => {
       return '$' + line.replace(/--/, '') + ': var(' + line + ');'})
     saveTokens('src/styles', 'tokens', scssClass, 'scss')
-    const cssClass = `:root {\n${cssVariables.replaceAll("--", "  --")}}\n`
-    saveTokens('build', 'tokens', cssClass, 'css')
 
-    let result = ''
-    fs.readdir(path.join('src/styles/'), function (err, files) {
-      //handling error
-      if (err) {
-          return console.log('Unable to scan directory: ' + err);
-      } 
-      //listing all files using forEach
-      files.forEach(function (file) {
-          // Do whatever you want to do with the file
-          result = result + sass.compile(`src/styles/${file}`, {style: "compressed"}).css;
-      });
-      // console.log(result); 
-      saveTokens('build', 'style', result, 'css')
-  });
   } catch (e) {
     console.log(
       "Provide a correct argument - a relative path to design tokens.\n",
