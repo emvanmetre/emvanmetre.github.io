b2083a993df76305c4ab7aa4a756fdccf719b0de
emvanmetre <emv4ew@virginia.edu>
2024-10-19 11:30:57 -0400

    fix: page export

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { Navbar } from './components'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages'
-import GHH from './pages'
+import { Home, GHH } from './pages'
 // import About from "./pages/about";
 // import Blogs from "./pages/blogs";
 // import SignUp from "./pages/signup";
