365ab1d594376c69fa0593c55657986289574813
emvanmetre <emv4ew@virginia.edu>
2024-10-19 11:33:11 -0400

    fix: names in ghh page

--- src/pages/ghh.tsx
+++ src/pages/ghh.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import GHH from 'ghh-24'
+import GHHProject from 'ghh-24'
 // import { Text, InnovateSVG, RetroGridSVG, RetroWindowSVG } from '../components/'
 // import { useMediaQuery } from 'react-responsive'
 
@@ -8,17 +8,7 @@ const GHH = () => {
 //   const isScreenSmall = useMediaQuery({ maxWidth: '1150px' })
   return (
     <>
-      {/* <div className={isScreenSmall ? "header-box sm" : "header-box"}>
-        <RetroWindowSVG classNames={isScreenSmall ? "retro-window-sm" : "retro-window"} />
-        <RetroGridSVG classNames={isScreenSmall ?'retro-grid-sm' : 'retro-grid'} />
-        <InnovateSVG classNames={'innovate-logo'} small={isScreenSmall} />
-      </div>
-      <div className="content">
-        <div className="content-bounded">
-          <Text size='lg' font='display'>Hmmm, this site is still a WIP...</Text>
-        </div>
-      </div> */}
-      <GHH></GHH>
+      <GHHProject></GHHProject>
     </>
   )
 }
