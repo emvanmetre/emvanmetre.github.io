fa8c07cc496124ec7d41cc2f932c7ff13d051d27
emvanmetre <elizabeth.vanmetre@vista.com>
2025-09-16 13:44:21 -0400

    fix: change vite build directory and deploy folder

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import githubPages from 'vite-plugin-github-pages'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), githubPages()],
+  base: '/',
   build: {
-    outDir: 'build', // CRA's default build output
+    outDir: 'dist', // CRA's default build output
   },
 })
