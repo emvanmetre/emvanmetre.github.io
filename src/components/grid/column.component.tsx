a59a3e68a1a0fa8128cc495e9bc4e20606dd6dc5
emvanmetre <elizabeth.vanmetre@vista.com>
2025-10-10 15:04:27 -0400

    feat: add possibility for column to have size prop

--- src/components/grid/column.component.tsx
+++ src/components/grid/column.component.tsx
@@ -3,14 +3,19 @@ import { CoreProps } from '../../core/types/utility'
 import { RenderComp } from '../core/render-comp.component'
 import { renderWithRef } from '../core/render-with-ref'
 
-type Props = {}
+type Props = {
+  size?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 // default is auto
+}
 
 export type ColumnProps = CoreProps<JSX.IntrinsicElements['div'], HTMLDivElement, Props>
 
 const Column = renderWithRef<MinNativeRef, ColumnProps>('Column', (props, ref) => {
-  const { children, ...rest } = props
+  const { children, size, ...rest } = props
 
   const classNames = new Set<string>(['grid-column'])
+  if (size) {
+    classNames.add(`grid-column-${size}`)
+  }
 
   return (
     <RenderComp root="div" classNames={classNames} forwardedRef={ref} props={{ ...rest }}>
